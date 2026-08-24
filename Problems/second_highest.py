467e798bd9cf33f660334999655fde514f4f1fda
varad42 <varadborole@gmail.com>
2026-08-24 18:05:22 +0530

    sec_largest

---
+++ Problems/second_highest.py
@@ -0,0 +1,21 @@
+def second_largest(numbers):
+    if len(numbers) < 2:
+        raise ValueError("need at least 2 elements to find a second largest")
+    elif numbers[0] > numbers[1]:
+        large_num = numbers[0]
+        sec_large_num = numbers[1]
+    else:
+        large_num = numbers[1]
+        sec_large_num = numbers[0]
+    for i in range(2, len(numbers)):
+        if numbers[i] > large_num:
+            sec_large_num = large_num
+            large_num = numbers[i]
+        elif numbers[i] > sec_large_num:
+            sec_large_num = numbers[i]
+
+    return sec_large_num
+print(second_largest([10, 5, 8, 20, 3]))  # 10
+print(second_largest([20, 10]))  # 10
+print(second_largest([5, 20, 20, 3]))  # ?  ← you decide: what SHOULD this give?
+print(second_largest([7]))
