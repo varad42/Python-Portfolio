55c0fb85f06d6bc7d8d3f8dd926020ffc77e8638
varad42 <varadborole@gmail.com>
2026-08-27 18:09:11 +0530

    reverse_in_list_hexaware_questions

---
+++ Problems/reverse_in_list.py
@@ -0,0 +1,21 @@
+# def reverse_in_place(numbers):
+#     left = 0
+#     right = len(numbers)-1
+#
+#     while left < right:
+#         temp = numbers[left]
+#         numbers[left] = numbers[right]
+#         numbers[right] = temp
+#         left += 1
+#         right -= 1
+#
+#     return numbers
+#
+# nums = [1,2,3,6]
+# print(reverse_in_place(nums))
+#
+#
+# nums1 = [1,2,3,4,5]
+# print(reverse_in_place(nums1))
+
+print(list(range(1, 4)))
