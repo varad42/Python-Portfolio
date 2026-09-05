629962dbad1d1a3cb3ab4e09c47234da29317385
varad42 <varadborole@gmail.com>
2026-09-05 11:49:52 +0530

    leetcode started

---
+++ leetcode/217_contains_duplicate.py
@@ -0,0 +1,5 @@
+# LeetCode 217 - Contains Duplicate
+# https://neetcode.io/problems/duplicate-integer/question?list=neetcode150
+# Time: O(n) | Space: O(n)
+# Approach: walk the list, keep a set of seen values;
+# a value already in the set means a duplicate.
---
+++ leetcode/242_valid_anagram.py
@@ -0,0 +1,5 @@
+# LeetCode 242_valid_anagram
+# https://neetcode.io/problems/is-anagram/question?list=neetcode150
+# Time: O(n) | Space: O(n)
+# Approach: walk the word.keep count of freq in dict, compare
+# if dict does'nt match False.
