94f3cd01de996de991ac7541a85795682bce1acd
varad42 <varadborole@gmail.com>
2026-09-06 18:13:37 +0530

    two sum

---
+++ leetcode/1_two_sum.py
@@ -0,0 +1,5 @@
+# Leetcode 01 - Two sum
+# https://neetcode.io/problems/two-integer-sum/history?list=neetcode150&submissionIndex=3
+# Time: O(n) | Space: O(n)
+# approach walk the list store every int check for the difference between target and current int inside stored one
+# difference between value and target match with value inside dict means got positions
